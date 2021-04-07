24ea765640523b022896f218df795bcab0d330e4
rock-007 <umair.ashraf2019@gmail.com>
2021-04-07 20:44:40 +0100

    Initial commit

---
+++ start_code/src/compare.py
@@ -0,0 +1,10 @@
+def compare(number_1, number_2):
+    if number_1 > number_2:
+        return f'{number_1} is greater than {number_2}'
+    elif number_2 > number_1:
+        return f'{number_1} is less than {number_2}'
+    elif number_2 == number_1:
+        return "numbers are the same"
+
+
+
